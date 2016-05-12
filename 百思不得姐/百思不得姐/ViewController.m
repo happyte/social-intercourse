6eb5130d13d5538c0c7f5e79c6c3164dcf19f3db
张树 <zhangshu@zhangshudemacbook-pro.local>
2016-05-12 15:36:49 +0800

    创建新项目

---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 张树 on 16/5/12.
+//  Copyright © 2016年 com.zs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"--------");
+}
+
+
+@end
---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by 张树 on 16/5/12.
+//  Copyright © 2016年 com.zs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
